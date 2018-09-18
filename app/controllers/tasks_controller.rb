f127f2e7e70e083446cff47bc564032063d77199
Trang <trang.frego@gmail.com>
2018-09-18 15:43:46 -0700

    complete baseline and wave 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
-TASKS = [
-  { task_desc: "Task One", status: "complete" },
-  { task_desc: "Task Two", status: "complete" },
-  { task_desc: "Task Three", status: "complete" },
-  { task_desc: "Task Four", status: "complete" }
-]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
+  end
+
+  def show
+    id = params[:id].to_i
+    @task = Task.find_by(id: id)
   end
 end
---
+++ db/migrate/20180918212758_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :description
+      t.string :completion_date
+
+      t.timestamps
+    end
+  end
+end
