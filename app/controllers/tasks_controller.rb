5e57dbc3005aef11141dcfac7d135a92b30e94dc
Trang <trang.frego@gmail.com>
2018-09-19 15:30:37 -0700

    add models, update routes, and add controller methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,8 +4,40 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completion_date: params[:task][:completion_date]) #instantiate a new book
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to root_path # go to the index so we can see the book in the list
+    else # save failed :(
+      render :new # show the new book form view again
+    end
+  end
+
   def show
     id = params[:id].to_i
     @task = Task.find_by(id: id)
+  end
+
+  def edit
+    id = params[:id].to_i
+    @task = Task.find_by(id: id)
+  end
+
+  def udpate
+    @task.name = params[:task][:name]
+    @task.description = params[:task][:description]
+    @task.completion_date = params[:task][:completion_date]
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to root_path # go to the index so we can see the book in the list
+    else # save failed :(
+      render :new # show the new book form view again
+    end
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
+
+  root 'tasks#index'
+  
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
+  # delete '/tasks/:id', to: 'tasks#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
