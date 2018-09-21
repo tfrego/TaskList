a01a319a3ad29d0bb49d7d292dca2e598e23c74f
Trang <trang.frego@gmail.com>
2018-09-21 13:27:39 -0700

    add resources and partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
 
   root 'tasks#index'
-
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create'
-
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   get 'tasks/:id/confirm', to: 'tasks#confirm', as: 'confirm_delete'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-
   patch '/tasks/:id/index', to: 'tasks#complete', as: 'complete_task'
-  patch '/tasks/:id', to: 'tasks#update'
+  resources :tasks
+  #
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # post '/tasks', to: 'tasks#create'
+  #
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 
-  delete '/tasks/:id', to: 'tasks#destroy'
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #
+
+  # patch '/tasks/:id', to: 'tasks#update'
+  #
+  # delete '/tasks/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
