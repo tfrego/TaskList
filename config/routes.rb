30b047a9ef9be15a190e2addf853c881226bc11e
Trang <trang.frego@gmail.com>
2018-09-21 14:10:01 -0700

    add confirm delete popup window

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,8 @@
 Rails.application.routes.draw do
 
   root 'tasks#index'
-  get 'tasks/:id/confirm', to: 'tasks#confirm', as: 'confirm_delete'
   patch '/tasks/:id/index', to: 'tasks#complete', as: 'complete_task'
   resources :tasks
-  #
-  # get '/tasks', to: 'tasks#index', as: 'tasks'
-  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  # post '/tasks', to: 'tasks#create'
-  #
-  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-
-  # get '/tasks/:id', to: 'tasks#show', as: 'task'
-  #
-
-  # patch '/tasks/:id', to: 'tasks#update'
-  #
-  # delete '/tasks/:id', to: 'tasks#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
